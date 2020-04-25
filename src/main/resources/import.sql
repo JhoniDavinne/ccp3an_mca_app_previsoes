f18d6358921532d7d0e90f58259909094efbbf69
JhoniDavinne <61547534+JhoniDavinne@users.noreply.github.com>
2020-04-25 19:43:59 -0300

    Previsão do Tempo Login H2 e Formulário

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,11 @@
 
-insert into previsoes_do_tempo(id, semana, temp_min, temp_max, umi, descricao) values(1, 'Domingo' , 20.9 , 26.2 , 55 , 'Céu limpo com poucas nuvens');
-insert into previsoes_do_tempo(id, semana, temp_min, temp_max, umi, descricao) values(2, 'Segunda-feira' , 17.3 , 20.0 , 46 , 'Sol');
-insert into previsoes_do_tempo(id, semana, temp_min, temp_max, umi, descricao) values(3, 'Terça-feira' , 21.5 , 24.4 , 60 , 'Parcialmente nublado');
-insert into previsoes_do_tempo(id, semana, temp_min, temp_max, umi, descricao) values(4, 'Quarta-feira' , 19.9 , 20.0 , 62 , 'Sol');
-insert into previsoes_do_tempo(id, semana, temp_min, temp_Max, umi, descricao) values(5, 'Quinta-feira' , 18.8 , 21.5 , 75 , 'Sol com algumas nuvens. Não chove.');
-insert into previsoes_do_tempo(id, semana, temp_min, temp_max, umi, descricao) values(6, 'Sexta-feira' , 17.5 , 19.0 , 64 , 'Parcialmente nublado');
-insert into previsoes_do_tempo(id, semana, temp_min, temp_max, umi, descricao) values(7, 'Sabado' , 19.2 , 22.5 , 67 , 'Céu Nublado');
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(1, '2020-04-26 12:00:00', 'Domingo' , 20.9 , 26.2 , 55 , 'Céu limpo com poucas nuvens', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(2, '2020-04-27 14:00:00', 'Segunda-feira' , 17.3 , 20.0 , 46 , 'Sol', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(3, '2020-04-28 22:00:00', 'Terça-feira' , 21.5 , 24.4 , 60 , 'Parcialmente nublado', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(4, '2020-04-29 20:00:00', 'Quarta-feira' , 19.9 , 20.0 , 62 , 'Sol', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_Max, umi, descricao, la, lo) values(5, '2020-04-30 17:00:00', 'Quinta-feira' , 18.8 , 21.5 , 75 , 'Sol com algumas nuvens. Não chove.', 20.0000, -20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(6, '2020-05-01 12:00:00', 'Sexta-feira' , 17.5 , 19.0 , 64 , 'Parcialmente nublado', 20.0000, -20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(7, '2020-05-01 19:00:00', 'Sabado' , 19.2 , 22.5 , 67 , 'Céu Nublado', 20.0000, -20.0000);
+
+insert into usuario(id, login, senha) values (1, 'admin', 'admin')
+insert into usuario(id, login, senha) values (2, 'aluno', '1234')
